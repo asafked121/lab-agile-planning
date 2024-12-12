76848ea0c27e0fbbd56053551f71c013b557965d
asafked121 <72229167+asafked121@users.noreply.github.com>
2024-12-11 17:16:01 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello world");
+# This is a simple Python script to calculate the factorial of a number
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Get input from the user
+num = int(input("Enter a number: "))
+
+# Calculate and print the factorial
+print("The factorial of", num, "is", factorial(num))
